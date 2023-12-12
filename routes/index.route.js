2aa081bf7bbf4cc90ffd121032ccdc9657a1cd83
Arizidane <samionohari@gmail.com>
2023-12-12 21:13:44 +0700

    fix: routes

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,21 +1,75 @@
 const productController = require('../controllers/product.controller');
 const userController = require('../controllers/user.controller');
+const Authorization = require('../middlewares/authorization.middleware');
 
 const router = require('express').Router();
 
-// product CRUD
-router.get('/api/products', productController.getAllProducts)
-router.get('/api/products/:id', productController.getProductByID)
-router.post('/api/products', productController.createProduct)
-router.put('/api/products/:id', productController.updateProductByID)
-router.delete('/api/products/:id', productController.deleteProductByID)
+// user login register
+router.post('/user/register', userController.register)
+router.post('/user/login', userController.login)
 
-// user CRUD *need bcrypt
-router.get('/api/users', userController.getAllUser)
-router.get('/api/users/:id', userController.getUserByID)
-router.post('/api/users', userController.createUser)
-router.put('/api/users/:id', userController.updateUserByID)
-router.delete('/api/users/:id', userController.deleteUserByID)
+// user CRUD
+// Read
+router.get(
+    '/api/users', 
+    Authorization.authorizationCustomer, 
+    Authorization.authorizationAdmin,
+    userController.getAllUser
+)
+// Read by ID
+router.get(
+    '/api/users/:id', 
+    Authorization.authorizationCustomer, 
+    Authorization.authorizationAdmin,
+    userController.getUserByID
+)
+// Create
+router.post(
+    '/api/users', 
+    Authorization.authorizationCustomer, 
+    Authorization.authorizationAdmin,
+    userController.createUser
+)
+// Update
+router.put(
+    '/api/users/:id', 
+    Authorization.authorizationCustomer, 
+    Authorization.authorizationAdmin,
+    userController.updateUserByID
+)
+// Delete
+router.delete(
+    '/api/users/:id', 
+    Authorization.authorizationCustomer, 
+    Authorization.authorizationAdmin,
+    userController.deleteUserByID
+)
+
+// product CRUD
+router.get(
+    '/api/products', 
+    productController.getAllProducts
+)
+router.get(
+    '/api/products/:id', 
+    productController.getProductByID
+)
+router.post(
+    '/api/products', 
+    Authorization.authorizationAdmin,
+    productController.createProduct
+)
+router.put(
+    '/api/products/:id', 
+    Authorization.authorizationAdmin,
+    productController.updateProductByID
+)
+router.delete(
+    '/api/products/:id', 
+    Authorization.authorizationAdmin,
+    productController.deleteProductByID
+)
+
 
 // order CRUD *need spesify userID
 // router.get('/api/orders',)
