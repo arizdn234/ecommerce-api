18a8475f523d71c55055ad3ad5bad8291062d328
Arizidane <samionohari@gmail.com>
2023-12-13 10:47:27 +0700

    feat:  payments controller

---
+++ controllers/payment.controller.js
@@ -0,0 +1,32 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+class PaymentController {
+  static async processPayment(req, res) {
+    try {
+      const { orderId, amount, paymentMethod } = req.body;
+
+      // Perform the payment processing here.
+
+      const paymentResult = {
+        status: 'success',
+        message: 'Payment processed successfully',
+        orderId,
+        amount,
+        paymentMethod,
+      };
+
+      await prisma.order.update({
+        where: { id: orderId },
+        data: { status: 'paid' },
+      });
+
+      res.status(200).json(paymentResult);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+}
+
+module.exports = PaymentController;
