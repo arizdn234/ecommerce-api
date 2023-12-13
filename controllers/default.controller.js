aeec1f31c86ea366ed745206a849b74a2b688e0a
Arizidane <samionohari@gmail.com>
2023-12-14 01:08:59 +0700

    feat: default controller

---
+++ controllers/default.controller.js
@@ -0,0 +1,21 @@
+class DefaultController {
+    static async underMaintenance(req, res) {
+        try {
+            res.status(503).json({ message: 'Sorry, this feature is under maintenance.' });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    }
+
+    static async landingPage(req, res) {
+        try {
+            res.status(200).json({ message: `Visit the API documentation at https://github.com/arizdn234/ecommerce-api?tab=readme-ov-file#dokumentasi.` });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    }
+  }
+  
+  module.exports = DefaultController;
