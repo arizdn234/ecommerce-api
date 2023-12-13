0660bc9deb46c3ce8c57e762863109503fb24bbd
Arizidane <samionohari@gmail.com>
2023-12-13 10:47:01 +0700

    fix: update order controller

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,130 +1,83 @@
-const { PrismaClient } = require("@prisma/client")
-const prisma = new PrismaClient()
+const { PrismaClient } = require('@prisma/client');
+const Order = new PrismaClient().order;
 
-class orderController {
-    static async getAllOrder (req, res) {
+class OrderController {
+    static async createOrder(req, res) {
         try {
-            const orders = await prisma.order.findMany()
-            res.status(200).json({
-                status: 'success',
-                data: orders
-            })
-        } catch (error) {
-            res.status(400).json({
-                status: 'failed',
-                message: error.message
-            })
-        }
-    }
-    
-    static async createOrder (req, res) {
-        try {
-            const { name, price, description } = req.body
-            const order = await prisma.order.create({ 
+            const { userId, totalAmount, status, orderItems } = req.body;
+            const newOrder = await Order.create({
                 data: {
-                    name,
-                    price,
-                    description 
-                }
-            }) 
-            res.status(201).json({
-                status: 'success',
-                data: order
-            })
-        } catch (error) {
-            res.status(400).json({
-                status: 'failed',
-                message: error.message
-            })
-        }
-    }
-
-    static async getOrderByID (req, res) {
-        try {
-            const orderID = req.params.id
-            const order = await prisma.order.findUnique({ 
-                where: { 
-                    id: parseInt(orderID) 
-                } 
-            })
-
-            if (!order) {
-                return res.status(404).json({
-                    status: 'failed',
-                    message: 'Order not found'
-                })
-            }
-            
-            res.status(200).json({
-                status: 'success',
-                data: order
-            })
-        } catch (error) {
-            res.status(400).json({
-                status: 'failed',
-                message: error.message
-            })
-        }
-    }
-
-    static async updateOrderByID (req, res) {
-        try {
-            const orderID = req.params.id
-            if (!await prisma.order.findUnique({ where: { id: parseInt(orderID) }})) {
-                return res.status(404).json({
-                    status: 'failed',
-                    message: 'Order not found'
-                })
-            }
-            const { name, price, description } = req.body
-            const order = await prisma.order.update({ 
-                where: { 
-                    id: parseInt(orderID) 
+                    userId,
+                    totalAmount,
+                    status,
+                    orderItems: {
+                        create: orderItems,
+                    },
                 },
-                data: {
-                    name,
-                    price,
-                    description
-                }
-            })
-            
-            res.status(200).json({
-                status: 'success',
-                data: order
-            })
+                include: {
+                    orderItems: true,
+                },
+            });
+            res.status(201).json(newOrder);
         } catch (error) {
-            res.status(400).json({
-                status: 'failed',
-                message: error.message
-            })
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
         }
     }
 
-    static async deleteOrderByID (req, res) {
+    static async getOrderById(req, res) {
         try {
-            const orderID = req.params.id
-            if (!await prisma.order.findUnique({ where: { id: parseInt(orderID) }})) {
-                return res.status(404).json({
-                    status: 'failed',
-                    message: 'Order not found'
-                })
+            const { id } = req.params;
+            const order = await Order.findUnique({
+                where: { id: parseInt(id) },
+                include: {
+                    orderItems: true,
+                },
+            });
+            if (!order) {
+                res.status(404).json({ error: 'Order not found' });
+            } else {
+                res.json(order);
             }
-            const deletedOrder = await prisma.order.delete({
-                where: { id: parseInt(orderID) },
-            })
-
-            res.status(200).json({
-                status: 'success',
-                message: 'Successfully deleted',
-                data: deletedOrder
-            })
         } catch (error) {
-            res.status(400).json({
-                status: 'failed',
-                message: error.message
-            })
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    }
+
+    static async getOrderByUserId(req, res) {
+        try {
+            const { userId } = req.params;
+            const orders = await Order.findMany({
+                where: { userId: parseInt(userId) },
+                include: {
+                    orderItems: true,
+                },
+            });
+            res.json(orders);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    }
+
+    static async updateOrder(req, res) {
+        try {
+            const { id } = req.params;
+            const { status } = req.body;
+            const updatedOrder = await Order.update({
+                where: { id: parseInt(id) },
+                data: { status },
+                include: {
+                    orderItems: true,
+                },
+            });
+            res.json(updatedOrder);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
         }
     }
 }
 
-module.exports = orderController
+module.exports = OrderController;
