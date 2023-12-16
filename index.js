efc91a4f0a867ad05cf9a0aaade46c2b1304a383
Arizidane <samionohari@gmail.com>
2023-12-17 01:41:02 +0700

    deployment config

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const router = require('./routes/index.route')
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(express.urlencoded())
 app.use(router);
 
